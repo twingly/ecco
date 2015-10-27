dbbe6bfc7e4d246fd93cdc90170addee1704a3bf
Johan, Mattias, Patrik, Robin <dev@twingly.com>
2015-10-27 18:04:38 +0100

    Fix platform in gemspec
    Require jar in module and keep track of external version.

--- lib/ecco.rb
+++ lib/ecco.rb
@@ -1,4 +1,5 @@
 require "ecco/version"
+require "ext/mysql-binlog-connector-java-#{Ecco::MYSQL_BINLOG_CONNECTOR_VERSION}.jar"
 
 module Ecco
   # Your code goes here...
--- lib/ecco/version.rb
+++ lib/ecco/version.rb
@@ -1,3 +1,4 @@
 module Ecco
-  VERSION = "0.1.0"
+  VERSION = "0.2.0"
+  MYSQL_BINLOG_CONNECTOR_VERSION = "0.2.4"
 end
