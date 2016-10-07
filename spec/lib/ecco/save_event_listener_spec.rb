f53bba55e2e8a65d5660f63a69bdaddfda7b2cf5
Patrik Ragnarsson <patrik@starkast.net>
2016-10-07 11:16:53 +0200

    More ordering

--- spec/lib/ecco/save_event_listener_spec.rb
+++ spec/lib/ecco/save_event_listener_spec.rb
@@ -19,20 +19,20 @@ describe Ecco::SaveEventListener do
     it "should return the binlog filename and position" do
       allow(subject).to receive(:accepted_events) { [event_type] }
 
-      acutal_event_type_name = nil
       actual_filename, actual_position = [nil, 0]
+      acutal_event_type_name = nil
 
       subject.callback = Proc.new do |filename, position, event_type_name|
-        acutal_event_type_name = event_type_name
         actual_filename = filename
         actual_position = position
+        acutal_event_type_name = event_type_name
       end
 
       subject.on_event(event)
 
-      expect(acutal_event_type_name).to eq("ROTATE")
       expect(actual_filename).to eq(binlog_filename)
       expect(actual_position).to eq(binlog_position)
+      expect(acutal_event_type_name).to eq("ROTATE")
     end
   end
 end
