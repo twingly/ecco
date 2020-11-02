b8a992785c27689b83aba3ca81ca69cfeb1e39ae
Chris <christofferhaj@gmail.com>
2020-11-02 15:05:47 +0100

    Implement RowType and RowTypeSave
    Implement new helper classes and clean up code.

--- lib/ecco/row_event_listener.rb
+++ lib/ecco/row_event_listener.rb
@@ -1,35 +1,21 @@
 require "ecco/event_listener"
 require "ecco/row_event"
+require_relative "row_type"
 
 module Ecco
   class RowEventListener < EventListener
-    # MySQL v1 and v2 row events
-    WRITE_EVENTS  = [EventType::WRITE_ROWS,  EventType::EXT_WRITE_ROWS]
-    UPDATE_EVENTS = [EventType::UPDATE_ROWS, EventType::EXT_UPDATE_ROWS]
-    DELETE_EVENTS = [EventType::DELETE_ROWS, EventType::EXT_DELETE_ROWS]
-
-    ROW_EVENTS  = WRITE_EVENTS + UPDATE_EVENTS + DELETE_EVENTS
-
-    def table_event
-      EventType::TABLE_MAP
-    end
-
-    def accepted_events
-      ROW_EVENTS
-    end
-
     def on_event(event)
       data = event.get_data
       type = event.get_header.get_event_type
 
       case type
-      when table_event
+      when Ecco::RowType::TABLE_EVENT
         @table_map_event = event
-      when *accepted_events
+      when *Ecco::RowType.accepted_events
         row_event          = Ecco::RowEvent.new
         row_event.table_id = data.get_table_id
         row_event.rows     = data.rows
-        row_event.type     = row_type_to_string(type)
+        row_event.type     = Ecco::RowType.fetch_event(type)
 
         if @table_map_event
           table_event_data = @table_map_event.get_data
@@ -39,18 +25,6 @@ module Ecco
         end
 
         @callback.call(row_event)
-      end
-    end
-
-    private
-
-    def row_type_to_string(type)
-      if WRITE_EVENTS.include?(type)
-        "WRITE_ROWS"
-      elsif UPDATE_EVENTS.include?(type)
-        "UPDATE_ROWS"
-      elsif DELETE_EVENTS.include?(type)
-        "DELETE_ROWS"
       end
     end
   end
--- lib/ecco/save_event_listener.rb
+++ lib/ecco/save_event_listener.rb
@@ -1,30 +1,16 @@
 require "ecco/event_listener"
+require_relative "row_type"
 
 module Ecco
   class SaveEventListener < EventListener
-    SAVE_EVENTS = {
-      EventType::QUERY           => "QUERY",
-      EventType::ROTATE          => "ROTATE",
-      EventType::WRITE_ROWS      => "WRITE_ROWS",
-      EventType::EXT_WRITE_ROWS  => "WRITE_ROWS",
-      EventType::UPDATE_ROWS     => "UPDATE_ROWS",
-      EventType::EXT_UPDATE_ROWS => "UPDATE_ROWS",
-      EventType::DELETE_ROWS     => "DELETE_ROWS",
-      EventType::EXT_DELETE_ROWS => "DELETE_ROWS",
-    }
-
-    def accepted_events
-      SAVE_EVENTS.keys
-    end
-
     def on_event(event)
       type = event.get_header.get_event_type
 
       case type
-      when *accepted_events
+      when *Ecco::RowTypeSave.accepted_events
         filename        = @client.get_binlog_filename
         position        = @client.get_binlog_position
-        event_type_name = SAVE_EVENTS.fetch(type)
+        event_type_name = Ecco::RowTypeSave.fetch_event(type)
 
         @callback.call(filename, position, event_type_name)
       end
