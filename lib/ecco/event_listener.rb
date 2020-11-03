67bfd8fcbaa3c53d731ab7527b6a4f5b5d59135b
Chris <christofferhaj@gmail.com>
2020-11-03 13:37:51 +0100

    Move RowType into EventListener

--- lib/ecco/event_listener.rb
+++ lib/ecco/event_listener.rb
@@ -13,5 +13,34 @@ module Ecco
     def on_event(event)
       raise NotImplementedError
     end
+
+    class RowType
+      # MySQL v1 and v2 row events
+      EVENTS = {
+        EventType::WRITE_ROWS      => "WRITE_ROWS",
+        EventType::EXT_WRITE_ROWS  => "WRITE_ROWS",
+        EventType::UPDATE_ROWS     => "UPDATE_ROWS",
+        EventType::EXT_UPDATE_ROWS => "UPDATE_ROWS",
+        EventType::DELETE_ROWS     => "DELETE_ROWS",
+        EventType::EXT_DELETE_ROWS => "DELETE_ROWS"
+      }
+
+      TABLE_EVENT = EventType::TABLE_MAP
+
+      def self.accepted_events
+        self::EVENTS.keys
+      end
+
+      def self.fetch_event(type)
+        self::EVENTS.fetch(type)
+      end
+    end
+
+    class RowTypeSave < RowType
+      EVENTS = {
+        EventType::QUERY  => "QUERY",
+        EventType::ROTATE => "ROTATE"
+      }.merge(EVENTS)
+    end
   end
 end
