9f9ea2f7a5e7416285e99e7e7b62edc2a797c049
Chris <38702971+Chrizpy@users.noreply.github.com>
2020-11-13 10:38:35 +0100

    Refactor event listeners (#58)
    Related to the issue: https://github.com/twingly/ecco/issues/25
    
    We move all the event hashes into `event_listener.rb`. 
    This way we don't have to define our events of interests in our child classes.
    We add some logic to the constructor of the class that will merge the event hashes
    of interest, to an instance variable. This way it is built and saved so it doesn't have to be
    done again, for the same object at least. We also save the keys in an instance array, which holds
    all the values that the keys point to in the hashes.
     
    Close  https://github.com/twingly/ecco/issues/25

--- lib/ecco/event_listener.rb
+++ lib/ecco/event_listener.rb
@@ -3,15 +3,35 @@ module Ecco
     include com.github.shyiko.mysql.binlog.BinaryLogClient::EventListener
     java_import com.github.shyiko.mysql.binlog.event.EventType
 
+    WRITE_EVENTS  = { EventType::WRITE_ROWS => "WRITE_ROWS", EventType::EXT_WRITE_ROWS => "WRITE_ROWS" }
+    UPDATE_EVENTS = { EventType::UPDATE_ROWS => "UPDATE_ROWS", EventType::EXT_UPDATE_ROWS => "UPDATE_ROWS" }
+    DELETE_EVENTS = { EventType::DELETE_ROWS => "DELETE_ROWS" , EventType::EXT_DELETE_ROWS => "DELETE_ROWS" }
+    QUERY_EVENTS  = { EventType::QUERY => "QUERY"}
+    ROTATE_EVENTS = { EventType::ROTATE => "ROTATE"}
+
+    TABLE_EVENT = EventType::TABLE_MAP
+
     attr_writer :callback
 
-    def initialize(client)
+    def initialize(client, events_of_interest)
       @client = client
+
+      @events_of_interest = events_of_interest.reduce(&:merge)
+      @accepted_events = @events_of_interest.keys
+
       @callback = Proc.new {}
     end
 
     def on_event(event)
       raise NotImplementedError
+    end
+
+    def accepted_events
+      @accepted_events
+    end
+
+    def event_type_to_string(type)
+      @events_of_interest.fetch(type)
     end
   end
 end
--- lib/ecco/save_event_listener.rb
+++ lib/ecco/save_event_listener.rb
@@ -2,32 +2,26 @@ require "ecco/event_listener"
 
 module Ecco
   class SaveEventListener < EventListener
-    SAVE_EVENTS = {
-      EventType::QUERY           => "QUERY",
-      EventType::ROTATE          => "ROTATE",
-      EventType::WRITE_ROWS      => "WRITE_ROWS",
-      EventType::EXT_WRITE_ROWS  => "WRITE_ROWS",
-      EventType::UPDATE_ROWS     => "UPDATE_ROWS",
-      EventType::EXT_UPDATE_ROWS => "UPDATE_ROWS",
-      EventType::DELETE_ROWS     => "DELETE_ROWS",
-      EventType::EXT_DELETE_ROWS => "DELETE_ROWS",
-    }
-
-    def accepted_events
-      SAVE_EVENTS.keys
+    def initialize(client)
+      super(client, events_of_interest)
     end
 
     def on_event(event)
       type = event.get_header.get_event_type
-
       case type
       when *accepted_events
         filename        = @client.get_binlog_filename
         position        = @client.get_binlog_position
-        event_type_name = SAVE_EVENTS.fetch(type)
+        event_type_name = event_type_to_string(type)
 
         @callback.call(filename, position, event_type_name)
       end
+    end
+
+    private
+
+    def events_of_interest
+      [WRITE_EVENTS, UPDATE_EVENTS, DELETE_EVENTS, QUERY_EVENTS, ROTATE_EVENTS]
     end
   end
 end
